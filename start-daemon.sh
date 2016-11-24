3497a41bc39e6765c259f3ca75e27d12984007bd
Jason Chan <snchan20@yahoo.com>
2016-11-24 12:41:26 -0800

    expose udp port for openvpn

--- start-daemon.sh
+++ start-daemon.sh
@@ -6,7 +6,7 @@ docker run -d --restart unless-stopped \
   -e HOST_LAN="192.168.1.0/24" \
   -e HOST_IP="172.17.0.1" \
   -p 8119:8119 \
-  -p 1194:1194 \
+  -p 1194:1194/udp \
   -v /etc/openvpn:/etc/openvpn \
   --cap-add NET_ADMIN \
   --dns=8.8.8.8 \
